c0dd05af75ca4eec6b6da42ed6d980134cd7fcf8
gunhoyoon <rkdus5964@gmail.com>
2023-08-20 19:37:38 +0900

    autoplay , infinite slid

--- src/components/feature.tsx
+++ src/components/feature.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import Practice from "../../public/blog/images/posts/best-react-practices.png";
 import ReactVersion from "../../public/blog/images/posts/react18-walkthrough.png";
 import Review_2023 from "../../public/blog/images/posts/review-2023.png";
-import NodeTip from "../../public/blog/images/posts/node-pro-tips.png";
-import JsTip from "../../public/blog/images/posts/javascript-10-tips.png";
+// import NodeTip from "../../public/blog/images/posts/node-pro-tips.png";
+// import JsTip from "../../public/blog/images/posts/javascript-10-tips.png";
 import Image from "next/image";
 import Link from "next/link";
 export default function Feature() {
@@ -11,7 +11,7 @@ export default function Feature() {
     <div className="mx-20 mb-10">
       <p className="font-bold mb-1">Featured Posts</p>
       <ul className="flex text-center gap-6">
-        <li className="w-4/12">
+        <li className="w-4/12 shadow-md rounded">
           <Link href="/">
             <div className="rounded-full">
               <Image
@@ -24,14 +24,17 @@ export default function Feature() {
                 <dt>2023년 리뷰</dt>
                 <dd>2023년 한해 동안 배운 기술들, 생각들 정리</dd>
               </dl>
-              <span className="bg-blue-200 p-1.5 text-xs rounded-xl">
+              <span
+                className="bg-blue-200 p-1.5 text-xs rounded-xl 
+inline-block mb-2"
+              >
                 my story
               </span>
             </div>
           </Link>
         </li>
-        <li className="w-4/12">
-          <Link href={`/posts/${1234}`}>
+        <li className="w-4/12 shadow-md rounded">
+          <Link href="/">
             <div>
               <Image
                 className="w-full h-50"
@@ -49,7 +52,7 @@ export default function Feature() {
             </div>
           </Link>
         </li>
-        <li className="w-4/12">
+        <li className="w-4/12 shadow-md rounded">
           <Link href="/">
             <div>
               <Image className="w-full h-50" src={Practice} alt="2023-Review" />
--- src/components/slide.tsx
+++ src/components/slide.tsx
@@ -7,6 +7,7 @@ import NodeBasic from "../../public/blog/images/posts/node-basic.png";
 import Review_2021 from "../../public/blog/images/posts/review-2021.png";
 import Review_2022 from "../../public/blog/images/posts/review-2022.png";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Slide() {
   const responsive = {
@@ -29,9 +30,14 @@ export default function Slide() {
     },
   };
   return (
-    <div className="mx-20 mb-20 text-center">
+    <div className="mx-20 mb-20 text-center ">
       <p className="text-left font-bold">You may like</p>
-      <Carousel responsive={responsive}>
+      <Carousel
+        responsive={responsive}
+        centerMode={true}
+        infinite={true}
+        autoPlay={true}
+      >
         <div>
           <Image src={JSbasic} alt="javascript-basic" />
           <p className="text-right">2022-08-31</p>
@@ -39,38 +45,53 @@ export default function Slide() {
             <dt>자바스크립트 기초 문법</dt>
             <dd>기본 문법부터 사용하는 실전까지 최종정리</dd>
           </dl>
-          <span className="bg-blue-200 p-1.5 text-xs rounded-xl">
+          <span className="bg-blue-200 p-1.5 text-xs rounded-xl inline-block mb-2">
             javascript
           </span>
         </div>
         <div>
-          <Image src={NodeBasic} alt="NodeBasic" />
-          <p className="text-right">2022-05-31</p>
-          <dl>
-            <dt>Node 기초 정리</dt>
-            <dd>기본적인 node 사용법에 대해 전부 정리!</dd>
-          </dl>
-          <span className="bg-blue-200 p-1.5 text-xs rounded-xl">backend</span>
+          <Link href="/">
+            <Image src={NodeBasic} alt="NodeBasic" />
+            <p className="text-right">2022-05-31</p>
+            <dl>
+              <dt>Node 기초 정리</dt>
+              <dd>기본적인 node 사용법에 대해 전부 정리!</dd>
+            </dl>
+            <span className="bg-blue-200 p-1.5 text-xs rounded-xl">
+              backend
+            </span>
+          </Link>
         </div>
         <div>
-          <Image src={Review_2021} alt="Review_2021" />
-          <p className="text-right">2021-02-31</p>
-          <dl>
-            <dt>2021년 리뷰</dt>
-            <dd>2021년 한 해 동안 배운 기술 리뷰!</dd>
-          </dl>
-          <span className="bg-blue-200 p-1.5 text-xs rounded-xl">my story</span>
+          <Link href="/">
+            <Image src={Review_2021} alt="Review_2021" />
+            <p className="text-right">2021-02-31</p>
+            <dl>
+              <dt>2021년 리뷰</dt>
+              <dd>2021년 한 해 동안 배운 기술 리뷰!</dd>
+            </dl>
+            <span className="bg-blue-200 p-1.5 text-xs rounded-xl">
+              my story
+            </span>
+          </Link>
         </div>
         <div>
-          <Image src={Review_2022} alt="Review_2022" />
-          <p className="text-right">2022-12-31</p>
-          <dl>
-            <dt>2022년 리뷰</dt>
-            <dd>2022년 한 해 동안 배운 기술 리뷰!</dd>
-          </dl>
-          <span className="bg-blue-200 p-1.5 text-xs rounded-xl">my story</span>
+          <Link href="/">
+            <Image src={Review_2022} alt="Review_2022" />
+            <p className="text-right">2022-12-31</p>
+            <dl>
+              <dt>2022년 리뷰</dt>
+              <dd>2022년 한 해 동안 배운 기술 리뷰!</dd>
+            </dl>
+            <span className="bg-blue-200 p-1.5 text-xs rounded-xl">
+              my story
+            </span>
+          </Link>
         </div>
       </Carousel>
     </div>
   );
 }
+
+// css shadow , 상세 페이지 구현 , 클릭시 상세 페이지 이동
+// 모든 페이지 푸터고정
